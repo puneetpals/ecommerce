e75292246a2533e6629a3066b05e636303c14c03
Puneet Pal Singh <85480695+puneetpals@users.noreply.github.com>
2024-09-05 22:54:42 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style type="text/css">
+.about{
+    width: 80%;
+    margin: auto;
+}
+.about h1{
+    font-size: 5em;
+    text-align: left;
+}
+.about-col{
+    flex-basis: 60%;
+}
+.about .row{
+    display: flex;
+    flex-direction: row;
+}
+.about-col img{
+    width: 100%;
+    margin-bottom: 60px;
+}
+    </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About us | Life Style Store</title>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+<body>
+
+<?php
+include 'includes/header.php';
+?>
+<div class="about">
+<div class="row">
+<div class="about-col">
+<h3><mark>WHO WE ARE</mark></h3><br>
+<p id="p1">Lifestyle store is a recognized leader in the ecommerce industry, with a steadfast commitment to success and a record of achievement that continues a tradition of delivering excellence.<br><br>The company was founded in 2015 by Vishwadutt M S to ease the process of ordering various items. From those early days with just one employee, Lifestyle Store now has more than 10000 employees, serving thousands of customers. <br><br>Lifestyle Store is guided by four principles: customer obsession rather than competitor focus, passion for invention, commitment to operational excellence, and long-term thinking.<br><br><h4><mark>Vision</mark></h4>To make the world a more stylish, colorful and happier place.<br><br><h4><mark>Mission</mark></h4>We strive to offer our customers the lowest possible prices, the best available selection, and the utmost convenience.
+</p>
+</div>
+<div class="about-col"><img src="img\team.jpg"></div>
+</div>
+</div>
+
+
+<div class="about">
+    <h3><mark>BUILDING THE FUTURE</mark></h3><br>
+    <p id="p1">We strive to have a positive impact on customers, employees, small businesses, the economy, and communities. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo eaque provident omnis repellat. Ex sit dignissimos sunt saepe. Dolore quibusdam odio unde adipisci repudiandae nemo debitis pariatur rem ratione hic.<br><br> We are smart, passionate builders with different backgrounds and goals, who share a common desire to always be learning and inventing on behalf of our customers.<br><br>We follow the following intensive growth strategies to match our competitors:<br>
+        <ul>
+        <li>Market development:<br>Our Focus is on entry and growth in new markets.</li><br>
+        <li>Market Penetration:<br>We aim to generate more revenue from markets where the company currently operates.</li><br>
+        <li>Product Development:<br>Our goal is to develop and offer new products to gain higher revenues. </li><br>
+        <li>Diversification:<br>Our objective is to achieve growth based on new business.</li><br> 
+        </ul>
+    </p>    
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
