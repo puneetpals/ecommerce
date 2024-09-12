27481b57e332e12224055088b6e8b178cde7da9b
Puneet Pal Singh <85480695+puneetpals@users.noreply.github.com>
2024-09-12 23:31:04 +0530

    Add files via upload
    Codes Update

---
+++ watches.php
@@ -0,0 +1,151 @@
+<?php
+//establish the connection to database, and start the session
+require("includes/common.php");
+?>
+
+<!--Specifies document type is html-->
+<!DOCTYPE html>
+<!--Specifies the language as English-->
+<html lang="en">
+    <head>
+        <!--instructs browser on how to control the page's dimensions and scaling-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--Title of products page-->
+        <title>Products | Life Style Store</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <?php
+        include 'includes/header.php';
+        include 'includes/check-if-added.php';
+        ?>
+        <div class="container" id="content">
+            <!-- Jumbotron Header -->
+            <div class="jumbotron home-spacer" id="products-jumbotron">
+                <h1>Welcome to our Lifestyle Store!</h1>
+                <p>We have the best cameras, watches and shirts for you. No need to hunt around, we have all in one place.</p>
+
+            </div>
+            <hr>
+
+                <div class="row text-center" id="watches">
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/18.jpg" alt="">
+                        <div class="caption">
+                            <h3>Titan Karishma Analog Multi-Colour Dial Men's Watch D011A</h3>
+                            <p>Price: Rs. 13000.00 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(5)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=5" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/19.jpg" alt="">
+                        <div class="caption">
+                            <h3>Titan Karishma Analog Black Dial Men's Watch TWHL41SMU07</h3>
+                            <p>Price: Rs. 3000.00 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(6)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=6" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/20.jpg" alt="">
+                        <div class="caption">
+                            <h3>HMT FASHION Watch for Men, Stainless Steel A97 Series</h3>
+                            <p>Price: Rs. 8000.00 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(7)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=7" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/21.jpg" alt="">
+                        <div class="caption">
+                            <h3>LOUIS DEVIN Silicone Strap Analog Wrist Watch HP877</h3>
+                            <p>Price: Rs. 18000.00 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(8)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=8" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr>
+        </div>
+
+        <?php include("includes/footer.php"); ?>
+    </body>
+
+</html>
