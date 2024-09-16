fd8fa97f1dd6f2a5214d1ade06e7abea9b0cc7c7
Puneet Pal Singh <85480695+puneetpals@users.noreply.github.com>
2024-09-16 22:26:32 +0530

    Add files via upload

---
+++ shirts.php
@@ -0,0 +1,152 @@
+<?php
+//establish the connection to database, and start the session
+require("includes/common.php");
+?>
+
+<!--Specifies document type is html-->
+<!DOCTYPE html>
+<!--Specifies the language as English-->
+<html lang="en">
+    <head>
+        <!--instructs browser on how to control the page's dimensions and scaling-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--Title of products page-->
+        <title>Products | Life Style Store</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <?php
+        include 'includes/header.php';
+        include 'includes/check-if-added.php';
+        ?>
+        <div class="container" id="content">
+            <!-- Jumbotron Header -->
+            <div class="jumbotron home-spacer" id="products-jumbotron">
+                <h1>Welcome to our Lifestyle Store!</h1>
+                <p>We have the best cameras, watches and shirts for you. No need to hunt around, we have all in one place.</p>
+
+            </div>
+            <hr>
+
+
+                <div class="row text-center" id="shirts">
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/22.jpg" alt="">
+                        <div class="caption">
+                            <h3>IndoPrimo Men's Cotton Casual Regular Fit Checks Shirt for Men</h3>
+                            <p>Price: Rs. 800.00 </p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(9)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=9" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/23.jpg" alt="">
+                        <div class="caption">
+                            <h3>U-TURN Men's Cotton Solid Formal/Semi Formal Shirt</h3>
+                            <p>Price: Rs. 1000.00</p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(10)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=10" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/24.jpg" alt="">
+                        <div class="caption">
+                            <h3>LookMark Men's Cotton Blend Printed Stitched Half Sleeve Shirt</h3>
+                            <p>Price: Rs. 1500.00</p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(11)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=11" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 home-feature">
+                    <div class="thumbnail">
+                        <img src="img/25.jpg" alt="">
+                        <div class="caption">
+                            <h3>Dennis Lingo Men's Solid Slim Fit Cotton Casual Shirt with Spread Collar</h3>
+                            <p>Price Rs. 1300.00</p>
+                            <!--User has to login to purchase the items-->
+                            <?php if (!isset($_SESSION['email'])) { ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            } else {
+                                //We have created a function to check whether this particular product is added to cart or not.
+                                if (check_if_added_to_cart(12)) { //This is same as if(check_if_added_to_cart != 0)
+                                    echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+                                } else {
+                                    ?>
+                                    <a href="cart-add.php?id=12" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <hr>
+        </div>
+
+        <?php include("includes/footer.php"); ?>
+    </body>
+
+</html>
